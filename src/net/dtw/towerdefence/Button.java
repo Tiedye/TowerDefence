46ea0c3b1923e64c44567ce2b36cd06ed33559c2
Tiedye <tiedye1@hotmail.com>
2015-06-01 13:40:02 -0400

    Interface work.

---
+++ src/net/dtw/towerdefence/Button.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.dtw.towerdefence;
+
+import net.dtw.math.Vec2d;
+
+/**
+ *
+ * @author Tiedye <tiedye1@hotmail.com>
+ */
+public class Button extends InterfaceObject {
+    
+    private String text;
+
+    public Button(String text, Vec2d pos) {
+        super();
+        this.text = text;
+    }
+    
+}
---
+++ src/net/dtw/towerdefence/InterfaceObject.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.dtw.towerdefence;
+
+import net.dtw.math.AABBd;
+import net.dtw.math.Vec2d;
+import net.dtw.render.Renderable;
+
+/**
+ *
+ * @author Tiedye <tiedye1@hotmail.com>
+ */
+public abstract class InterfaceObject implements Renderable {
+    
+    protected AABBd bounds;
+    protected boolean dragging;
+    protected int zOrder;
+    protected boolean dead;
+    
+    public InterfaceObject (){
+        bounds = new AABBd(0, 0, 0, 0);
+        dragging = false;
+        zOrder = 0;
+        dead = false;
+    }
+    
+    public int getZOrdering(){
+        return zOrder;
+    }
+    
+    public AABBd getBoundingBox(){
+        return bounds;
+    }
+    
+    public boolean onMouseDown(Vec2d pos, int button){
+        return false;
+    }
+    public boolean onMouseUp(Vec2d pos, int button){
+        return false;
+    }
+    public boolean onMouseMove(Vec2d finalPos, Vec2d delta){
+        return false;
+    }
+    
+    public boolean isDragging(){
+        return dragging;
+    }
+    public boolean drag(Vec2d finalPos, Vec2d delta){
+        return false;
+    }
+    
+    public boolean onKeyDown(int key){
+        return false;
+    }
+    public boolean onKeyUp(int key){
+        return false;
+    }
+    
+    public void setDead(){
+        dead = true;
+    }
+    
+    protected int vaoID;
+
+    @Override
+    public void constructRenderable() {
+        
+    }
+
+    @Override
+    public void destructRenderable() {
+        
+    }
+
+    @Override
+    public void render() {
+        
+    }
+
+    @Override
+    public boolean isDead() {
+        return dead;
+    }
+    
+}
